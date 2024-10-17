058a788c9f3ddd3e61c53f2a51d3f5fbf4b63e2e
ASUS <ASUS@DESKTOP-NB7MT4D>
2024-10-17 20:08:25 +0400

    AllFileDownTCP

---
+++ Client/Program.cs
@@ -0,0 +1,49 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+class TcpClientProj
+{
+    static void Main(string[] args)
+    {
+        var endPoint = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 58758);//change port
+        using (var client = new TcpClient())
+        {
+            client.Connect(endPoint);
+            if (client.Connected)
+            {
+                using (var stream = client.GetStream())
+                {
+                    var path = @"C:\Users\ASUS\Desktop\MAUI.txt";
+
+                    if (File.Exists(path))
+                    {
+                        var file = new FileInfo(path);
+
+                        var fileNameBytes = Encoding.UTF8.GetBytes(file.Name);
+                        stream.Write(fileNameBytes, 0, fileNameBytes.Length);
+
+                        var fileLengthBytes = Encoding.UTF8.GetBytes(file.Length.ToString());
+                        stream.Write(fileLengthBytes, 0, fileLengthBytes.Length);
+
+                        using (var fs = new FileStream(path, FileMode.Open, FileAccess.Read))
+                        {
+                            var bytes = new byte[1024];
+                            int len;
+                            while ((len = fs.Read(bytes, 0, bytes.Length)) > 0)
+                            {
+                                stream.Write(bytes, 0, len);
+                            }
+                        }
+                        Console.WriteLine($"File sent: {file.Name}");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"File not found: {path}");
+                    }
+                }
+            }
+            else { Console.WriteLine("Try Again Not Connected"); }
+        }
+    }
+}
---
+++ TCPFileSentForAllFileExtentionServer/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+class TcpServer
+{
+    static void Main(string[] args)
+    {
+        var server = new TcpListener(IPAddress.Parse("127.0.0.1"), 58758);
+        server.Start();
+        Console.WriteLine("Server is running...");
+
+        while (true)
+        {
+            using (var client = server.AcceptTcpClient())
+            using (var stream = client.GetStream())
+            {
+                var fileNameBuffer = new byte[1024];
+                int fileNameLength = stream.Read(fileNameBuffer, 0, fileNameBuffer.Length);
+                var fileName = Encoding.UTF8.GetString(fileNameBuffer, 0, fileNameLength);
+
+                var fileLengthBuffer = new byte[1024];
+                int fileLengthBytes = stream.Read(fileLengthBuffer, 0, fileLengthBuffer.Length);
+                int fileLength = int.Parse(Encoding.UTF8.GetString(fileLengthBuffer, 0, fileLengthBytes));
+
+                var path1 = "C:\\Users\\ASUS\\Desktop\\NewFolderForAllFiles"; //Change Path For You Comp
+                if (!Directory.Exists(path1)) {
+                    Directory.CreateDirectory(path1);
+                }
+
+                var path = $"{path1}\\{DateTime.Now:HH.mm.ss}{Path.GetExtension(fileName)}";
+
+                using (var fileStream = new FileStream(path, FileMode.Create, FileAccess.Write))
+                {
+                    var totalReceivedBytes = 0;
+                    var buffer = new byte[1024];
+
+                    while (totalReceivedBytes < fileLength)
+                    {
+                        int bytesRead = stream.Read(buffer, 0, buffer.Length);
+                        fileStream.Write(buffer, 0, bytesRead);
+                        totalReceivedBytes += bytesRead;
+                    }
+                }
+
+                Console.WriteLine($"File downloaded: {path}");
+            }
+        }
+    }
+}
